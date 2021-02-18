f439244b32900391946dcdbb13d05269175f2d8f
ifer-itcast <yangkangkang@itcast.cn>
2021-02-18 16:26:55 +0800

    Vuex基本使用

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{count}}
+    <hr>
+    <button @click="handleClick1">+8</button>
+    <button @click="handleClick2">+8 async</button>
+    <hr>
+    {{filterList}}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mapState, mapMutations, mapActions, mapGetters } from 'vuex'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  computed: {
+    // !#1 获取 Vuex 中 state 的 2 种方式
+    /* count() {
+      return this.$store.state.count
+    } */
+    ...mapState(['count']),
+    // !#2 获取 Vuex 中 getters 的 2 种方式
+    /* filterList() {
+      return this.$store.getters.filterList;
+    }, */
+    ...mapGetters(['filterList'])
+  },
+  methods: {
+    handleClick1() {
+      // !#3 触发 Vuex 中 mutations 的 2 种方式
+      // this.$store.commit('addCount', 8)
+      this.addCount(8)
+    },
+    ...mapMutations(['addCount']),
+    handleClick2() {
+      // !#4 触发 Vuex 中 actions 的 2 种方式
+      // this.$store.dispatch('addCountAsync', 8)
+      this.addCountAsync(8)
+    },
+    ...mapActions(['addCountAsync'])
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+// #1
+import Vuex from 'vuex';
+Vue.use(Vuex);
 
-Vue.config.productionTip = false
+//#2
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+    list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  },
+  mutations: {
+    addCount(state, payload) {
+      state.count += payload;
+    },
+  },
+  actions: {
+    addCountAsync(context, payload) {
+      setTimeout(() => {
+        context.commit('addCount', payload);
+      }, 1000);
+    },
+  },
+  getters: {
+    /* filterList(state) {
+      return state.list.filter(item => item > 5)
+    } */
+    filterList: state => state.list.filter(item => item > 5),
+  },
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  // #3
+  store,
+}).$mount('#app');
